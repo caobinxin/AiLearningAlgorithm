c1e1af523ce9fe4ef187b865773fdc608369d6d4
caobinxin <caobinxin666@163.com>
2019-05-11 14:59:42 +0800

    改变目录结构

---
+++ 00.env/env-test.py
@@ -0,0 +1,21 @@
+# _*_ coding: utf-8 _*_
+
+import io
+import sys
+
+sys.stdout=io.TextIOWrapper(sys.stdout.buffer,encoding='utf8')
+import numpy as np 
+print("欢迎加入")
+
+import pandas as pd
+print("slightech")
+
+import matplotlib.pyplot as plt
+print("Ａｎｄｒｏｉｄ　组")
+
+
+from sklearn.decomposition import PCA
+print("study")
+
+import tensorflow as tf 
+print("直通车")
